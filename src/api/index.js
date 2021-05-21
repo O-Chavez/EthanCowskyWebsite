574a17574fde52d956a3893779dbd26328d577ab
Oscar <oscar.a.chavez.jr@gmail.com>
2021-05-20 20:35:41 -0700

    thought i added this change already but added heroku api link

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,5 @@
-export const api = 'http://localhost:3001';
+// FOR DEVELOPMENT
+// export const api = 'http://localhost:3001';
+
+// FOR DEPLOYMENT
+export const api = 'https://ethan-cowsky.herokuapp.com/';
