221724fa2f96c9534fb18a6dd43d604cf612cc12
Oscar <oscar.a.chavez.jr@gmail.com>
2021-05-20 21:42:38 -0700

    changed api file to remove additional '/'

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@
 // export const api = 'http://localhost:3001';
 
 // FOR DEPLOYMENT
-export const api = 'https://ethan-cowsky.herokuapp.com/';
+export const api = 'https://ethan-cowsky.herokuapp.com';
 
 // this is just to see if there are actually any changes that get pushed to heroku...
